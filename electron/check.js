8f0eb060cc4d1e7646864dbc7f876f17e52c63fb
Lubos-source <luboshorky2@seznam.cz>
2022-08-30 16:02:17 +0200

    update of desktop app

--- electron/check.js
+++ electron/check.js
@@ -1,3 +1,6 @@
+var flagerion;
+
+
 $.fn.existChecker = function(){
 return this.each(function(){
     
@@ -19,6 +22,12 @@ return this.each(function(){
 
                         //pouzit checker
                         //checking agains forbiden chacters :)
+                    const argon2 = require('argon2-browser');
+                       argon2.verify({ pass: ''+selfValue+'', encoded: '$argon2d$v=19$m=1024,t=1,p=1$aW50ZXJlc3RpbmdsdWJvc3NvdXJjZXNhbHQ$ODZCX5YvxVLe79/kqu8v/w1A7bEudvWP' })
+    .then(() => {console.log('OK');feedback.text("You GOT it !");})
+    .catch(e => {console.error(e.message, e.code);feedback.text("NAH wrong one !");console.log("val : "+selfValue)})
+//chnage long hash by my secret hashes
+//if the has has been already found, somehow delete it ? or remember it.
 
                     }
                 }
